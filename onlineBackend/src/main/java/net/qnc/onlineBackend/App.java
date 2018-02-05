1076db5fa22c6c144a37f02387ee97f892020a2e
Quinton Lephoto <quinctancred@yahoo.com>
2018-02-05 13:56:45 +0200

    My project set up

---
+++ onlineBackend/src/main/java/net/qnc/onlineBackend/App.java
@@ -0,0 +1,13 @@
+package net.qnc.onlineBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
